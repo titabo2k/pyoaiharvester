16888ff7816d0c1181a1aedb5ec5dffbd8790313
titabo2k <titabo2k@users.noreply.github.com>
2019-07-16 17:34:22 +0200

    some updates, we use urllib, argparse and logging now; special url escaping in case of lex base

--- pyoaiharvest.py
+++ pyoaiharvest.py
@@ -1,92 +1,140 @@
-import sys
-import urllib2
+import urllib.request
+import urllib.parse
 import zlib
 import time
 import re
 import xml.dom.pulldom
-import operator
-import codecs
-from optparse import OptionParser
+import argparse
+import logging
 
 nDataBytes, nRawBytes, nRecoveries, maxRecoveries = 0, 0, 0, 3
 
 
-def getFile(serverString, command, verbose=1, sleepTime=0):
+def getData(serverString, command, lexBASE, verbose=1, sleepTime=0):
     global nRecoveries, nDataBytes, nRawBytes
     if sleepTime:
         time.sleep(sleepTime)
+
+    if lexBASE:
+        command = urllib.parse.quote(command, safe=lexBASE, encoding=None, errors=None)
+
     remoteAddr = serverString + '?verb=%s' % command
+    
     if verbose:
-        print "\r", "getFile ...'%s'" % remoteAddr[-90:]
-    headers = {'User-Agent': 'OAIHarvester/2.0', 'Accept': 'text/html',
-               'Accept-Encoding': 'compress, deflate'}
+        logging.info("getData ...'{0}'".format(remoteAddr))
+
     try:
-        #remoteData=urllib2.urlopen(urllib2.Request(remoteAddr, None, headers)).read()
-        remoteData = urllib2.urlopen(remoteAddr).read()
-    except urllib2.HTTPError, exValue:
+        with urllib.request.urlopen(remoteAddr) as resp:
+            remoteData = str(resp.read().decode('utf-8'))
+    except urllib.request.HTTPError as exValue:
         if exValue.code == 503:
             retryWait = int(exValue.hdrs.get("Retry-After", "-1"))
             if retryWait < 0:
                 return None
-            print 'Waiting %d seconds' % retryWait
-            return getFile(serverString, command, 0, retryWait)
-        print exValue
+            logging.info("Waiting {:d} seconds".format(retryWait))
+            return getData(serverString, command, 0, retryWait)
+        logging.warn("http error {0} occured".format(exValue))
         if nRecoveries < maxRecoveries:
             nRecoveries += 1
-            return getFile(serverString, command, 1, 60)
+            return getData(serverString, command, 1, 60)
         return
+    
     nRawBytes += len(remoteData)
+    
     try:
         remoteData = zlib.decompressobj().decompress(remoteData)
     except:
         pass
+    
     nDataBytes += len(remoteData)
+    
     mo = re.search('<error *code=\"([^"]*)">(.*)</error>', remoteData)
+
+    eRT = re.search('<ListRecords/>', remoteData)
+    
     if mo:
-        print "OAIERROR: code=%s '%s'" % (mo.group(1), mo.group(2))
+        logging.warn("OAIERROR: code={0} '{1}'".format(mo.group(1), mo.group(2)))
+    if eRT and lexBASE:
+        logging.info("base repo empty records list")
     else:
         return remoteData
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-10s %(message)s", 
+        level=logging.INFO,
+    )
 
-    parser = OptionParser()
+    parser = argparse.ArgumentParser('pyoaiharvest')
 
-    parser.add_option("-l", "--link", dest="link", help="URL of repository")
-    parser.add_option("-o", "--filename", dest="filename", help="write repository to file")
-    parser.add_option("-f", "--from", dest="fromDate", help="harvest records from this date yyyy-mm-dd")
-    parser.add_option("-u", "--until", dest="until", help="harvest records until this date yyyy-mm-dd")
-    parser.add_option("-m", "--mdprefix", dest="mdprefix", default="oai_dc", help="use the specified metadata format")
-    parser.add_option("-s", "--setName", dest="setName", help="harvest the specified set")
+    parser.add_argument(
+        "-uri", 
+        "--baseURI",  
+        help="base URI of the repository",
+        required=True,
+    )
+    parser.add_argument(
+        "-o", 
+        "--filename", 
+        help="write repository to file",
+        required=True,
+    )
+    parser.add_argument(
+        "-f", 
+        "--from", 
+        dest="fromDate",
+        help="harvest records from this date yyyy-mm-dd",
+    )
+    parser.add_argument(
+        "-u", 
+        "--until", 
+        help="harvest records until this date yyyy-mm-dd",
+    )
+    parser.add_argument(
+        "-m", 
+        "--mdprefix", 
+        default="oai_dc", 
+        help="use the specified metadata format prefix",
+    )
+    parser.add_argument(
+        "-s", 
+        "--setName",  
+        help="harvest the specified set",
+    )
+    parser.add_argument(
+        "--lexBASE", 
+        action='store_const',
+        const='=()&:+',
+        help="base relatet url escaping for '=()&:+'",
+    )
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    if options.link is None or options.filename is None:
-        parser.print_help()
-        parser.error("a repository url and output file are required")
-
-    if options:
-        serverString = verbOpts = fromDate = untilDate = mdPrefix = oaiSet = ''
-        if options.link:
-            serverString = options.link
-        if options.filename:
-            outFileName = options.filename
-        if options.fromDate:
-            fromDate = options.fromDate
-        if options.until:
-            untilDate = options.until
-        if options.mdprefix:
-            mdPrefix = options.mdprefix
-        if options.setName:
-            oaiSet = options.setName
+    if args:
+        serverString = verbOpts = fromDate = untilDate = mdPrefix = oaiSet = lexBASE = ''
+        if args.baseURI:
+            serverString = args.baseURI
+        if args.filename:
+            outFileName = args.filename
+        if args.fromDate:
+            fromDate = args.fromDate
+        if args.until:
+            untilDate = args.until
+        if args.mdprefix:
+            mdPrefix = args.mdprefix
+        if args.setName:
+            oaiSet = args.setName
+        if args.lexBASE:    
+            lexBASE = args.lexBASE
     else:
-        print usage
+        print(usage)
 
-    if not serverString.startswith('http'):
-        serverString = 'http://' + serverString
+    logging.info("Writing records to {0} from archive {1}".format(
+        outFileName,
+        serverString,
+    ))
 
-    print "Writing records to %s from archive %s" % (outFileName, serverString)
-
-    ofile = codecs.lookup('utf-8')[-1](file(outFileName, 'wb'))
+    ofile = open(outFileName,"w")
 
     ofile.write('<repository xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" \
      xmlns:dc="http://purl.org/dc/elements/1.1/" \
@@ -100,12 +148,8 @@ if __name__ == "__main__":
         verbOpts += '&until=%s' % untilDate
     if mdPrefix:
         verbOpts += '&metadataPrefix=%s' % mdPrefix
-    else:
-        verbOpts += '&metadataPrefix=%s' % 'oai_dc'
 
-    print "Using url:%s" % serverString + '?ListRecords' + verbOpts
-
-    data = getFile(serverString, 'ListRecords' + verbOpts)
+    data = getData(serverString, 'ListRecords' + verbOpts, lexBASE)
 
     recordCount = 0
 
@@ -119,10 +163,14 @@ if __name__ == "__main__":
         mo = re.search('<resumptionToken[^>]*>(.*)</resumptionToken>', data)
         if not mo:
             break
-        data = getFile(serverString, "ListRecords&resumptionToken=%s" % mo.group(1))
+        
+        data = getData(serverString, "ListRecords&resumptionToken=%s" % mo.group(1), lexBASE)
 
     ofile.write('\n</repository>\n'), ofile.close()
 
-    print "\nRead %d bytes (%.2f compression)" % (nDataBytes, float(nDataBytes) / nRawBytes)
+    logging.info("Read {:d} bytes ({:.2f} compression)".format(
+        nDataBytes,
+        (float(nDataBytes)/nRawBytes),
+    ))
 
-    print "Wrote out %d records" % recordCount
+    logging.info("Wrote out {:d} records".format(recordCount))
